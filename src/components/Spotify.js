666f839a203a5d989099fc1e818a20c16058ece6
KristinaRomero <kristiromer@gmail.com>
2025-01-21 15:36:23 -0500

    Fix Spotify callback redirection

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -4,28 +4,30 @@ let accessToken;
 
 
 const Spotify = {
-  getAccessToken() {
-    if (accessToken) {
-      return accessToken;
-    }
-
-    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-
-    if (accessTokenMatch && expiresInMatch) {
-      accessToken = accessTokenMatch[1];
-      const expiresIn = Number(expiresInMatch[1]);
-
-      // Clear the access token after it expires
-      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
-      window.history.pushState("Access Token", null, "/");
-      return accessToken;
-    } else {
-      // Redirect to Spotify authorization
-      const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
-      window.location = authUrl;
-    }
-  },
+    getAccessToken() {
+      if (accessToken) {
+        return accessToken;
+      }
+  
+      // Check for access token and expiration in the URL
+      const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+      const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+  
+      if (accessTokenMatch && expiresInMatch) {
+        accessToken = accessTokenMatch[1];
+        const expiresIn = Number(expiresInMatch[1]);
+  
+        // Clear the access token after it expires
+        window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+        window.history.pushState("Access Token", null, "/Jamming-App/"); // Ensure we redirect back to the app's root
+  
+        return accessToken;
+      } else {
+        // Redirect to Spotify authorization
+        const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+        window.location = authUrl;
+      }
+    },
 
   search(term) {
     const accessToken = Spotify.getAccessToken();
